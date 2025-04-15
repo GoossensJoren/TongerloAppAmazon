f4b90b2b368332b0831774113944dc02147cef00
unknown <joren.goossens1@telenet.be>
2025-04-15 17:50:21 +0200

    changed sponsors & small info fixes

--- polyfills-es2015.js
+++ polyfills-es2015.js
@@ -6466,8 +6466,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Projects\tongerloCodeOld\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! C:\Projects\tongerloCodeOld\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! C:\projects\tongerloCode\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\projects\tongerloCode\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
